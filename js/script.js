eb68e33450c862ce7e27047d4cf3e01595235dd4
matheuskrs <angelblast220@gmail.com>
2023-11-30 16:44:37 -0300

    Adição de duas novas paginas

--- js/script.js
+++ js/script.js
@@ -1,18 +1,15 @@
-function mensagem(){
-    alert("Ola");
-}
-
 let usuarios = [
-    { nome: "Matheus", foto: "img/avatar3.png" },
-    { nome: "Giovanna", foto: "img/avatar6.jpg" },
-    { nome: "Gallo", foto: "img/avatar1.png" }
+    { nome: "Gallo", foto: "img/avatar1.png" },
+    { nome: "Capitão", foto: "img/avatar4.png" },
+    { nome: "TriZóio", foto: "img/avatar5.jpg" }
 ];
+//console.log(usuarios);
+
 let ul = document.querySelector('ul');
 
-console.log(ul);
 ul.innerHTML = '';
 
-for(let i = 0; i < usuarios.length; i++){
+for (let i = 0; i < usuarios.length; i++) {
     ul.innerHTML += `<li>
                         <a href="#">
                             <div class="profile">
@@ -22,11 +19,12 @@ for(let i = 0; i < usuarios.length; i++){
                         </a>
                     </li>`;
 }
+
 ul.innerHTML += `<li>
-                    <a href="#">
+                    <a href="adicionar.html">
                         <div class="profile">
                             <i class='bx bxs-plus-circle'></i>
-                            <span>Adicionar Perfil</span>
+                            <span>Adicionar perfil</span>
                         </div>
                     </a>
                 </li>`;
